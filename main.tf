f6d6503f2a153778ad742990cf4f4e75aebd33cc
jenniferasuwata <jennifer.asuwata@gmail.com>
2024-10-19 15:51:57 +0100

    Added new variables

--- main.tf
+++ main.tf
@@ -2,30 +2,94 @@ provider "aws" {
  region = var.region
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy     = "default"
+  tags = {
+    name = "ApacheVPC"
+  }
+}
+
+data "aws_ssm_parameter" "instance_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
 resource "aws_security_group" "cba_tf_sg" {
- name        = "cba_tf_sg"
- description = "allow all traffic"
- ingress {
-   from_port  = 22
-   to_port    = 22
-   protocol   = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
-   from_port  = 80
-   to_port    = 80
-   protocol   = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
- egress {
-   from_port  = 0
-   to_port    = 0
-   protocol   = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
- tags = {
-   name = "CBAterraformSG"
- }
+  name        = "cba_tf_sg"
+  vpc_id      = aws_vpc.my_vpc.id
+  description = "allow all traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    name = "CBAterraformSG"
+  }
+}
+
+resource "aws_subnet" "cba_public" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-west-1a"
+
+  tags = {
+    Name = "ApachePublicSubnet"
+  }
+}
+
+resource "aws_subnet" "cba_private" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-west-1b"
+
+  tags = {
+    Name = "ApachePrivateSubnet"
+  }
+}
+
+resource "aws_internet_gateway" "cba_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "ApacheIGW"
+  }
+}
+
+resource "aws_route_table" "cba_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cba_igw.id
+  }
+
+  tags = {
+    "Name" = "ApachePublicRT"
+  }
+
+}
+
+resource "aws_route_table_association" "cba_subnet_rt_public" {
+  subnet_id      = aws_subnet.cba_public.id
+  route_table_id = aws_route_table.cba_public_rt.id
 }
 
 data "aws_key_pair" "sample_kp" {
@@ -33,13 +97,16 @@ data "aws_key_pair" "sample_kp" {
 }
 
 resource "aws_instance" "cba_tf_instance" {
+  ami = data.aws_ssm_parameter.instance_ami.value
   instance_type = var.instance_type
-  security_groups = [aws_security_group.cba_tf_sg.name]
-  ami = var.instance_ami
+  subnet_id = aws_subnet.cba_public.id
+  security_groups = [aws_security_group.cba_tf_sg.id]
   key_name = var.key_name
-  user_data = file("install_apache.sh")
+  user_data = fileexists("install_apache.sh") ? file("install_apache.sh") : null
 
   tags = {
-    Name = "CBATerraformInstance"
+    Name = "ApacheInstance"
     }
 }
+
+data "aws_region" "current" {}
--- variables.tf
+++ variables.tf
@@ -6,16 +6,6 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-02f64c390601e5f36"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
-
 variable "key_name" {
   default = "cba_admin_Ireland"
 }
